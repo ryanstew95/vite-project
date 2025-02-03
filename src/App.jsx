72165ce336f06ec04942514c61848ec8dfece8f3
Ryan Stewart <ryanstew17@gmail.com>
2025-02-03 13:14:00 -0400

    working on set up for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Navbar from './componets/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-     <h1>hello world!</h1>
+<Navbar />
     </div>
   )
 }
