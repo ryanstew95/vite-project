e5a6099bdaec5896d2f5e03a0db25efa720dc9c6
Ryan Stewart <ryanstew17@gmail.com>
2025-02-24 15:10:29 -0400

    sytax issuein navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
-import  Navbar  from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Menu from "./components/Menu/Menu";
 import Home from "./components/Home/Home";
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -69,3 +69,5 @@ export const Navbar = () => {
     </nav>
   );
 };
+
+export default Navbar;
